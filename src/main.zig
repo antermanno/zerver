e9d3c368f6f7637a0eb78ebc72d29870446ba64b
MrBong <thepowerofthebong@gmail.com>
2026-05-04 18:37:07 +0200

    UDP socket that reads user messages

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
 const Io = std.Io;
+const net = Io.net;
+const log = std.log;
 
 const zerver = @import("zerver");
 
+// print function for debugging reasons
+const print = std.debug.print;
+
 pub fn main(init: std.process.Init) !void {
     // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    print("All your {s} are belong to us.\n", .{"codebase"});
 
     // This is appropriate for anything that lives as long as the process.
     const arena: std.mem.Allocator = init.arena.allocator();
@@ -19,16 +24,38 @@ pub fn main(init: std.process.Init) !void {
     // In order to do I/O operations need an `Io` instance.
     const io = init.io;
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    // // Stdout is for the actual output of your application, for example if you
+    // // are implementing gzip, then only the compressed bytes should be sent to
+    // // stdout, not any debugging messages.
+    // var stdout_buffer: [1024]u8 = undefined;
+    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    // const stdout_writer = &stdout_file_writer.interface;
+    //
+    // try zerver.printAnotherMessage(stdout_writer);
+    //
+    // try stdout_writer.flush(); // Don't forget to flush!
+    //
 
-    try zerver.printAnotherMessage(stdout_writer);
+    // Try to start a socket
+    const addr = try net.IpAddress.parseIp4("127.0.0.1", 10999);
+    const sock = try addr.bind(io, .{ .mode = .dgram, .protocol = .udp });
+    defer sock.close(io);
 
-    try stdout_writer.flush(); // Don't forget to flush!
+    // Create a buffer for the socket to write
+    var buf: [1024]u8 = undefined;
+
+    log.info("Socket listening on port {d}\n", .{addr.getPort()});
+
+    // Let the socket receive the data, it blocks.
+    // After receiving the data the program should close
+    while (true) {
+        const msg = try sock.receive(io, &buf);
+        if (std.mem.eql(u8, msg.data, "close\n")) break;
+        log.info(
+            "received {d} byte(s) from {f};\n    string: {s}\n",
+            .{ msg.data.len, msg.from, msg.data },
+        );
+    }
 }
 
 test "simple test" {
