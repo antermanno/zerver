8144745e6b9cc190e8d71c5c2e4b1eabaa07c8dc
MrBong <thepowerofthebong@gmail.com>
2026-05-18 20:29:05 +0200

    Managed to run htmx

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,42 @@
 const std = @import("std");
 const Io = std.Io;
 const net = Io.net;
+const http = std.http;
 const log = std.log;
 
 const zerver = @import("zerver");
+
+const HTML_TEMPLATE =
+    \\ <html>
+    \\ <title> My Cute Website </title>
+    \\  <p><b>This is a serious Website</b></p>
+    \\   <body>
+    \\      Long long ago there was some text
+    \\     <p><a href="/static">for my cutie</a></p>
+    \\     <p><a href="/htmx">surprises</a></p>
+    \\   </body>
+    \\ </html>
+;
+
+const HTML_STATIC =
+    \\ <html>
+    \\ <title> My Cute Website </title>
+    \\   <body>
+    \\ <p>Hi Theeeeeeee!!!!!! <3<3<3<3<3<3<3<3</p>
+    \\ <p>(^_^*)</p>
+    \\     <p><a href="/">homepage</a></p>
+    \\   </body>
+    \\ </html>
+;
+
+const HTMX =
+    \\  <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.10/dist/htmx.min.js" crossorigin="anonymous"></script>
+    \\ <div id="response-div"></div>
+    \\  <!-- have a button POST a click via AJAX -->
+    \\ <button hx-post="/content" hx-target="#response-div" hx-swap="innerHTML">
+    \\ Click Me For A Surprise
+    \\  </button>
+;
 
 // print function for debugging reasons
 const print = std.debug.print;
@@ -24,44 +57,115 @@ pub fn main(init: std.process.Init) !void {
     // In order to do I/O operations need an `Io` instance.
     const io = init.io;
 
-    // // Stdout is for the actual output of your application, for example if you
-    // // are implementing gzip, then only the compressed bytes should be sent to
-    // // stdout, not any debugging messages.
-    // var stdout_buffer: [1024]u8 = undefined;
-    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    // const stdout_writer = &stdout_file_writer.interface;
-    //
-    // try zerver.printAnotherMessage(stdout_writer);
-    //
-    // try stdout_writer.flush(); // Don't forget to flush!
-    //
+    // Try to start a server
+    const addr = try net.IpAddress.parseIp4("0.0.0.0", 9999);
 
-    // Try to start a socket
-    const addr = try net.IpAddress.parseIp4("127.0.0.1", 10999);
-    const sock = try addr.bind(io, .{ .mode = .dgram, .protocol = .udp });
-    defer sock.close(io);
+    var server = try addr.listen(io, .{ .reuse_address = true, .protocol = .tcp });
+    defer server.deinit(io); // Release resources with the server
+    defer server.socket.close(io); // Close the socket
 
-    // Create a buffer for the socket to write
-    var buf: [1024]u8 = undefined;
+    print("This is a {any} and it looks like this: {any}\n", .{ @TypeOf(server), server });
 
-    log.info("Socket listening on port {d}\n", .{addr.getPort()});
+    log.info("Now listening on port {}\n", .{addr.ip4.port});
 
-    // Let the socket receive the data, it blocks.
-    // After receiving the data the program should close
+    // Main loop
     while (true) {
-        const msg = try sock.receive(io, &buf);
-        if (std.mem.eql(u8, msg.data, "close\n")) break;
-        log.info(
-            "received {d} byte(s) from {f};\n    string: {s}\n",
-            .{ msg.data.len, msg.from, msg.data },
-        );
+
+        // Accept a connection
+        const conn: net.Stream = try server.accept(io);
+        // defer conn.close(io);
+
+        log.info("Oops! Someone connected, better to say HI\n", .{});
+
+        // Do something with the connection
+        // Concurrently
+        _ = try io.concurrent(handler, .{ io, conn });
     }
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn handler(io: Io, conn: net.Stream) !void {
+
+    // close the connection
+    defer conn.close(io);
+
+    // Start the server with the appropriate reader and writer
+    //
+    // I suppose a server is an API on top of a Stream (which is indeed a "tcp-like" socket
+    // that provides reader and writer interfaces
+    var rbuf: [4 * 1024]u8 = undefined;
+    var wbuf: [1024]u8 = undefined;
+
+    // We pass the addresses of the buffers to coerce them into slices
+    var reader = conn.reader(io, &rbuf);
+    var writer = conn.writer(io, &wbuf);
+
+    var server = http.Server.init(&reader.interface, &writer.interface);
+
+    // Server loop
+    while (server.reader.state == .ready) {
+        // We handle the request for a specific conecction close connection if there are errors
+        var request = server.receiveHead() catch |err| switch (err) {
+            error.HttpConnectionClosing => return,
+            error.HttpHeadersOversize => {
+                log.warn("Headers too big", .{});
+                return;
+            },
+            else => {
+                log.warn("Error with the request: {any}\n", .{err});
+                return err;
+            },
+        };
+        log.info("Responding to {any}\n", .{request.head.method});
+        log.info("Responding to {s}\n", .{request.head.target});
+
+        if (request.head.method != .GET and request.head.method != .POST) {
+            request.respond("403", .{ .reason = "Unauthorized" }) catch break;
+            break;
+        }
+        // for now we only authorize get method on the first two routes
+
+        // let us switch on the path
+        const Route = enum { home, static, htmx, content, invalid };
+
+        // Possibly make it authomatic
+        var API_ENDPOINT: Route = .invalid;
+
+        if (std.mem.eql(u8, request.head.target, "/")) API_ENDPOINT = .home;
+        if (std.mem.eql(u8, request.head.target, "/static")) API_ENDPOINT = .static;
+        if (std.mem.eql(u8, request.head.target, "/htmx")) API_ENDPOINT = .htmx;
+        if (std.mem.eql(u8, request.head.target, "/content")) API_ENDPOINT = .content;
+
+        switch (API_ENDPOINT) {
+            .home => request.respond(HTML_TEMPLATE, .{ .status = .ok, .extra_headers = &.{
+                .{ .name = "Fake Header", .value = "Fake Value" },
+            } }) catch |err| {
+                log.warn("Error for the response: {any}\n", .{err});
+                break;
+            },
+            .static => request.respond(HTML_STATIC, .{ .status = .ok, .extra_headers = &.{
+                .{ .name = "Fake Header", .value = "Fake Value" },
+            } }) catch |err| {
+                log.warn("Error for the response: {any}\n", .{err});
+                break;
+            },
+            .htmx => request.respond(HTMX, .{ .status = .ok, .extra_headers = &.{
+                .{ .name = "Fake Header", .value = "Fake Value" },
+            } }) catch |err| {
+                log.warn("Error for the response: {any}\n", .{err});
+                break;
+            },
+            .content => request.respond("U are my cutie! <3", .{ .status = .ok, .extra_headers = &.{
+                .{ .name = "Fake Header", .value = "Fake Value" },
+            } }) catch |err| {
+                log.warn("Error for the response: {any}\n", .{err});
+                break;
+            },
+            .invalid => {
+                request.respond("404", .{ .reason = "Bad URI" }) catch break;
+                break;
+            },
+        }
+    }
 }
+
+test "simple test" {}
